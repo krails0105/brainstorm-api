b4dc2f0d11ec3893d2c7e8613230d6f8d1fcc010
shkim <shkim@cryptoquant.com>
2026-03-17 22:17:54 +0900

    feature: disable csrf

--- src/main/java/com/brainstorm/brainstorm_api/entity/Room.java
+++ src/main/java/com/brainstorm/brainstorm_api/entity/Room.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import java.time.LocalDateTime;
 import lombok.NoArgsConstructor;
 
@@ -22,4 +23,11 @@ public class Room {
     private String topic;
 
     private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        if (this.createdAt == null) {
+            this.createdAt = LocalDateTime.now();
+        }
+    }
 }
